4f5f4c47c7b7896333b4099f09ad659b6b22aad3
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2022-08-06 12:41:01 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Web/WebApiTest/RichClientApp/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace RichClientApp
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private OrdersClientModel model = new OrdersClientModel();
+
+        public string? _statusMessage;
+        public string? StatusMessage
+        {
+            get => _statusMessage;
+            set => SetProperty(ref _statusMessage, value);
+        }
+
+        public List<OrderResource>? _orders;
+        public List<OrderResource>? Orders
+        {
+            get => _orders;
+            set => SetProperty(ref _orders, value);
+        }
+
+        public OrderResource _currentOrder = new();
+        public OrderResource CurrentOrder
+        {
+            get => _currentOrder;
+            set => SetProperty(ref _currentOrder, value);
+        }
+
+        private async void ExecuteLoadInvoice()
+        {
+            try
+            {
+                Orders = await model.GetOrdersAsync(_currentOrder.CustomerId);
+                StatusMessage = null;
+            }
+            catch
+            {
+                Orders = null;
+                StatusMessage = "No orders for this customer!";
+            }
+        }
+        public RelayCommand LoadInvoice => new RelayCommand(ExecuteLoadInvoice);
+
+        private async Task ExecuteSubmitOrder()
+        {
+            int orderNo = await model.PostOrderAsync(_currentOrder);
+            if (orderNo != 0)
+                StatusMessage = $"New order number is {orderNo}";
+            else
+                StatusMessage = "Order Failed!";
+        }
+        public AsyncRelayCommand SubmitOrder => new AsyncRelayCommand(ExecuteSubmitOrder);
+    }
+}
---
+++ CodingSamples/DotNet/Web/WebApiTest/RichClientApp/OrdersClientModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace RichClientApp
+{
+    public class OrderResource
+    {
+        public int OrderNo { get; set; }
+
+        public string? OrderDate { get; set; }
+
+        public string CustomerId { get; set; } = null!;
+
+        public int ProductNo { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+
+    public class OrdersClientModel
+    {
+        private HttpClient client = new HttpClient();
+
+        public async Task<List<OrderResource>?> GetOrdersAsync(string customerId)
+        {
+            return await client.GetFromJsonAsync<List<OrderResource>>($"{Settings.Default.ServiceUri}{customerId}");
+        }
+
+        public async Task<int> PostOrderAsync(OrderResource resource)
+        {
+            var response = await client.PostAsJsonAsync(Settings.Default.ServiceUri, resource);
+            if (response.IsSuccessStatusCode)
+            {
+                var order = await response.Content.ReadFromJsonAsync<OrderResource>();
+                return order?.OrderNo ?? 0;
+            }
+            return 0;
+        }
+    }
+}
